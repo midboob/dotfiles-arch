6319006e87044b0fde89898680850b827f79c229
midboob <edwarddan72@gmail.com>
2025-05-08 20:47:16 -0700

    added nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- neopywal/matugen
+local neopywal = require("neopywal")
+neopywal.setup()
+vim.cmd.colorscheme("neopywal")
