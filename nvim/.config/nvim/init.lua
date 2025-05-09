07036a828096d2eaf979d5146fbd0c0300766db0
midboob <edwarddan72@gmail.com>
2025-05-08 20:54:49 -0700

    removed nvim config

--- nvim/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- neopywal/matugen
-local neopywal = require("neopywal")
-neopywal.setup()
-vim.cmd.colorscheme("neopywal")
--- nvim/.config/nvim/lua/plugins/image.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "3rd/image.nvim",
-    dependencies = { "luarocks.nvim" },
-    opts = {},
-  },
-}
