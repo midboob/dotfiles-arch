b6e4058d6d843c5e0a9b729d48b8dff8100c3209
midboob <edwarddan72@gmail.com>
2025-05-09 17:57:50 -0700

    edit nvim config

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -6,6 +6,11 @@ return {
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_forward_search_on_start = false
+      vim.g.vimtex_compiler_latexmk = {
+        aux_dir = "/home/edward/.texfiles/",
+        out_dir = "/home/edward/.texfiles/",
+      }
     end,
   },
 }
