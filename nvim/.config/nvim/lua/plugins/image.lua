4e0fe93985560e429e5418e1e8a152beacb8e49a
midboob <edwarddan72@gmail.com>
2025-05-12 14:35:19 -0700

    edit nvim config

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -2,6 +2,18 @@ return {
   {
     "3rd/image.nvim",
     dependencies = { "luarocks.nvim" },
-    opts = {},
+    opts = {
+      backend = "kitty",
+      processor = "magick_cli",
+      resolve_image_path = function(document_path, image_path, fallback)
+        local working_dir = vim.fn.getcwd()
+        -- Format image path for Obsidian notes
+        if working_dir:find("/mnt/Storage/Documents/My Safe/") then
+          return working_dir .. "/" .. image_path
+        end
+        -- Fallback to the default behavior
+        return fallback(document_path, image_path)
+      end,
+    },
   },
 }
