b8a62c17f885df78b31fe145bb95d68a731529b6
midboob <edwarddan72@gmail.com>
2025-05-18 21:41:48 -0700

    edit nvim config

--- nvim/.config/nvim/lua/plugins/neopywal.lua
+++ nvim/.config/nvim/lua/plugins/neopywal.lua
@@ -1,9 +1,7 @@
 return {
-  {
-    "RedsXDD/neopywal.nvim",
-    name = "neopywal",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+  "RedsXDD/neopywal.nvim",
+  name = "neopywal",
+  lazy = false,
+  priority = 1000,
+  opts = {},
 }
