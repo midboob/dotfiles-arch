cec6ca00e73a8b7721d0429d50b863319c1f4a29
midboob <edwarddan72@gmail.com>
2025-05-23 02:13:38 -0700

    nvim fixed mostly

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- neopywal/matugen
-local neopywal = require("neopywal")
-neopywal.setup()
-vim.cmd.colorscheme("neopywal")
-
 -- clipboard
 vim.opt.clipboard = "unnamedplus"
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+local opt = vim.opt
+
 --  always enable wrap
-vim.opt.wrap = true
+opt.wrap = true
+
+-- conceal options
+-- opt.conceallevel = 2
