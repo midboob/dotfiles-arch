ead7cc891d728f753b80dc96a0447440cfed4dcc
midboob <edwarddan72@gmail.com>
2025-07-06 19:51:18 -0700

    feat: ✨ some stuff changes

--- nvim/.config/nvim/lua/plugins/extras/coding/blink-extended.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/blink-extended.lua
@@ -13,5 +13,13 @@ return {
         },
       },
     },
+    keymap = {
+      preset = "super-tab",
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
+    },
   },
 }
